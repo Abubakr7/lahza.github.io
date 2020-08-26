59012fa050c9618e1da9fd23235d4048019d7ae4
Abubakr Juraev <33040137+Abubakr7@users.noreply.github.com>
2020-08-26 14:42:59 +0500

    Lahza
    Landing Page "Lahza"

---
+++ precache-manifest.9302a03897d7b68c917eeafd94b0b139.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de915f22154c77f47c9f0d5ce52aea71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "126dc7067c96f26080a1",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "0acb0bfec76d02892bed",
+    "url": "/static/css/main.f490eca2.chunk.css"
+  },
+  {
+    "revision": "126dc7067c96f26080a1",
+    "url": "/static/js/2.c3003436.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c3003436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0acb0bfec76d02892bed",
+    "url": "/static/js/main.99a6ec0f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "6188d388f387fa06fae23d63ab22023a",
+    "url": "/static/media/Mask Group (1).6188d388.png"
+  },
+  {
+    "revision": "0146ad0ff1cfdcd3cce624c888cd4239",
+    "url": "/static/media/facebook.0146ad0f.svg"
+  },
+  {
+    "revision": "815dcaf690a235f1748f5262cf855102",
+    "url": "/static/media/instagramm.815dcaf6.svg"
+  },
+  {
+    "revision": "1c42b19a6bdc1fb7680c96cbd7913ea6",
+    "url": "/static/media/lahza.1c42b19a.png"
+  },
+  {
+    "revision": "891b25d2608e3527a4b250e852278991",
+    "url": "/static/media/lahzaAuto.891b25d2.png"
+  },
+  {
+    "revision": "4d1f3c9daf5ba7a5aa3db0d7fd1656e8",
+    "url": "/static/media/logo.4d1f3c9d.svg"
+  },
+  {
+    "revision": "997ca82cd9eb21579818c2401d9afb0a",
+    "url": "/static/media/mini.997ca82c.svg"
+  },
+  {
+    "revision": "84b9250162b6c2086ca524ab293bc0d8",
+    "url": "/static/media/next.84b92501.svg"
+  },
+  {
+    "revision": "2f9ae29fe1b4b0d2d1bfc2bed1f0d38f",
+    "url": "/static/media/next_white.2f9ae29f.svg"
+  },
+  {
+    "revision": "d3c7a17af2a352453908b3c894fab2ff",
+    "url": "/static/media/passport.d3c7a17a.svg"
+  },
+  {
+    "revision": "41754bd4231c6d2fc552a4a8d92811da",
+    "url": "/static/media/send.41754bd4.svg"
+  },
+  {
+    "revision": "764d5f89875aafb8766a9f1bb365ab1f",
+    "url": "/static/media/telegramm.764d5f89.svg"
+  },
+  {
+    "revision": "dc20f10141276885a76f12cd31e5a9fd",
+    "url": "/static/media/vk.dc20f101.svg"
+  }
+]);
